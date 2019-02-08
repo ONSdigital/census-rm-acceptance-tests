c467257bdc071013810713ca5b7e3f81c930f21a
Hugh Brace <hugh.ons@gmail.com>
2019-02-08 13:31:40 +0000

    create mandatory events for collection exercise

--- utilities/collection_exercise_utilities.py
+++ utilities/collection_exercise_utilities.py
@@ -35,3 +35,43 @@ def get_collection_exercise_id_from_response(response):
 def delete_collection_exercise(id):
     # TODO: DELETE /collectionexercises/id
     pass
+
+
+def create_collex_event(collection_exercise_id, event_tag, timestamp):
+    logger.debug('Adding a collection exercise event',
+                 collection_exercise_id=collection_exercise_id, event_tag=event_tag)
+
+    collex_event_url = f"{Config.COLLECTION_EXERCISE_SERVICE}/collectionexercises/{collection_exercise_id}/events"
+    eventTag = {
+        "tag": event_tag,
+        "timestamp": timestamp
+    }
+
+    response = requests.post(collex_event_url, auth=Config.BASIC_AUTH, json=eventTag)
+    response.raise_for_status()
+    if response.status_code == 201:
+        logger.debug('Event created', collection_exercise_id=collection_exercise_id, event_tag=event_tag)
+    return response.status_code
+
+
+def create_mandatory_events(collection_exercise_id, collex_events: dict):
+    logger.debug('Creating mandatory events', collection_exercise_id=collection_exercise_id)
+    collex_event_url = f"{Config.COLLECTION_EXERCISE_SERVICE}/collectionexercises/{collection_exercise_id}/events"
+
+    return_statuses = []
+
+    for event, timestamp in collex_events.items():
+        event_tag = {
+            'tag': event,
+            'timestamp': timestamp
+        }
+        response = requests.post(collex_event_url, auth=Config.BASIC_AUTH, json=event_tag)
+        response.raise_for_status()
+        if response.status_code == 201:
+            logger.debug('Event created', collection_exercise_id=collection_exercise_id, event_tag=event_tag)
+        else:
+            logger.debug('Event not created', collection_exercise_id=collection_exercise_id, event_tag=event_tag)
+
+        return_statuses.append(response.status_code)
+
+    return return_statuses
