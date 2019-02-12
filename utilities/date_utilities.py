7c1d12a008d265f8b5fc5189feaeaf4356cca84d
Richard Weeks <richard.weeks@ons.gov.uk>
2019-02-12 08:11:57 +0000

    Automatically generate datestamps.

--- utilities/date_utilities.py
+++ utilities/date_utilities.py
@@ -8,10 +8,11 @@ def get_timestamp():
     return maya.now().iso8601()
 
 
-def get_timestamp_with_offset(months, weeks):
-    future = datetime.now() + relativedelta(months=months, weeks=weeks)
-    return maya.MayaDT.from_datetime(future).iso8601()
-
+def get_timestamp_with_offset(months=0, weeks=0, days=0, hours=0, minutes=0, seconds=0, microseconds=0):
+    future = datetime.now() + relativedelta(months=months, weeks=weeks, days=days, hours=hours,
+                                            minutes=minutes, seconds=seconds, microseconds=microseconds)
+    date_str = future.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+    return date_str + 'Z'
 
 # Example date string to parse is 'April 29 2018 05:15'
 def parse_timestamp(date):
