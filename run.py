1c51e9c341304bd13d263ad5d9dbe477d111ddfc
AdeHarrison <adrian.harrison.uk@gmail.com>
2019-02-25 13:40:13 +0000

    Pass logging level from makefile to behave

--- run.py
+++ run.py
@@ -1,8 +1,36 @@
-import os
+import argparse
+import logging
 
 from behave import __main__ as behave_executable
 
-if __name__ == '__main__':
-    behave_format = os.getenv('BEHAVE_FORMAT', 'progress2')
+DEFAULT_LOG_LEVEL = 'INFO'
+DEFAULT_BEHAVE_FORMAT = 'progress2'
+DEFAULT_FEATURE_DIRECTORY = 'acceptance_tests/features'
 
-    behave_executable.main(args=f'--format {behave_format} acceptance_tests/features')
+
+def parse_arguments():
+    """
+    Parses commandline arguments
+    :return: Parsed arguments
+    """
+    parser = argparse.ArgumentParser('Run behave scenarios')
+    parser.add_argument('--log_level', '-l', help='Logging level', default=DEFAULT_LOG_LEVEL)
+    parser.add_argument('--format', '-f', help='Behave format', default=DEFAULT_BEHAVE_FORMAT)
+    parser.add_argument('--feature_directory', '-fd', help='Feature directory', default=DEFAULT_FEATURE_DIRECTORY)
+
+    return parser.parse_args()
+
+
+def main():
+    """
+    Runner
+    """
+    args = parse_arguments()
+
+    logging.basicConfig(level=args.log_level)
+
+    behave_executable.main(args=f'--logging-level {args.log_level} --format {args.format} {args.feature_directory}')
+
+
+if __name__ == '__main__':
+    main()
