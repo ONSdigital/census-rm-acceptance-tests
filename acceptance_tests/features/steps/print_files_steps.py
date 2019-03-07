279fbd8f413b61ebf273574f5e7ee36418e59964
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-03-07 07:39:33 +0000

    refactors a little

--- acceptance_tests/features/steps/print_files_steps.py
+++ acceptance_tests/features/steps/print_files_steps.py
@@ -14,13 +14,10 @@ use_step_matcher("re")
 logger = wrap_logger(logging.getLogger(__name__))
 
 
-# This test stores quite a bit of data incore.
-# For large file size performance will deteriorate and then kaboom!
-# So performance style tests should work differently, not sure how...
 @step("the a correctly formatted file is created on the sftp server")
 def check_correct_files_on_sftp_server(context):
     sftp_client = create_open_sftp_client()
-    context.cases = _get_cases_with_iacs_for_sample_units(context)
+    _get_iacs_and_apply_to_sample_units(context)
     expected_csv_lines = _create_expected_csv_lines(context)
 
     _check_notification_files_have_all_the_expected_data(sftp_client,
@@ -30,13 +27,37 @@ def check_correct_files_on_sftp_server(context):
                                                          expected_csv_lines)
 
 
-def _create_expected_csv_lines(context):
-    _add_iac_to_sample_units(context.sample_units, context.cases)
+def _get_iacs_and_apply_to_sample_units(context):
+    cases = _get_cases_with_iacs_for_sample_units(context)
+    _add_iac_to_sample_units(context.sample_units, cases)
 
+
+def _create_expected_csv_lines(context):
     return [
         _create_expected_csv_line(expected_data, context.collex_return_by_date)
         for expected_data in context.sample_units
     ]
+
+
+@retry(retry_on_exception=lambda e: isinstance(e, FileNotFoundError),
+       wait_fixed=5000, stop_max_attempt_number=24)
+def _check_notification_files_have_all_the_expected_data(sftp_client, start_of_test, survey_ref, period,
+                                                         expected_csv_lines):
+    logger.debug('Checking for files on SFTP server')
+
+    files = _get_files_filtered_by_survey_ref_period_and_modified_date(sftp_client, survey_ref, period, start_of_test)
+    if len(files) == 0:
+        raise FileNotFoundError
+
+    actual_content_list = _get_files_content_as_list(sftp_client, files)
+
+    if set(actual_content_list) != set(expected_csv_lines):
+        file_names = [f.filename for f in files]
+        logger.info('Unable to find all iacs', files_found=file_names, expected_csv_lines=expected_csv_lines,
+                    actual_content_list=actual_content_list)
+        raise FileNotFoundError
+
+    return True
 
 
 def _create_expected_csv_line(expected_data, return_by_date):
@@ -67,7 +88,7 @@ def _get_iac_for_sample_unit(sample_unit_id, cases):
             return case["iac"]
 
     # or raise an exception here?
-    return f"iac not found in for sample unit id {sample_unit_id}"
+    return f"iac not found for sample unit id {sample_unit_id}"
 
 
 def _get_cases_with_iacs_for_sample_units(context):
@@ -82,26 +103,6 @@ def _get_cases_with_iacs_for_sample_units(context):
         cases_with_iac.append(case)
 
     return cases_with_iac
-
-
-@retry(retry_on_exception=lambda e: isinstance(e, FileNotFoundError),
-       wait_fixed=5000, stop_max_attempt_number=24)
-def _check_notification_files_have_all_the_expected_data(sftp_client, start_of_test, survey_ref, period, expected_csv_lines):
-    logger.debug('Checking for files on SFTP server')
-
-    files = _get_files_filtered_by_survey_ref_period_and_modified_date(sftp_client, survey_ref, period, start_of_test)
-    if len(files) == 0:
-        raise FileNotFoundError
-
-    actual_content_list = _get_files_content_as_list(sftp_client, files)
-
-    if set(actual_content_list) != set(expected_csv_lines):
-        file_names = [f.filename for f in files]
-        logger.info('Unable to find all iacs', files_found=file_names, expected_csv_lines=expected_csv_lines,
-                    actual_content_list=actual_content_list)
-        raise FileNotFoundError
-
-    return True
 
 
 def _get_files_content_as_list(sftp_client, files):
@@ -124,6 +125,7 @@ def _get_file_lines_as_list(sftp_client, file):
 def _get_files_filtered_by_survey_ref_period_and_modified_date(client, survey_ref, period, start_of_test):
     files = client.listdir_attr(Config.SFTP_DIR)
     start_of_test = _round_to_minute(start_of_test)
+
     return list(filter(lambda f: f'{survey_ref}_{period}' in f.filename
                                  and start_of_test <= datetime.fromtimestamp(f.st_mtime), files))
 
