40cf23517c299ad325ab724ce77abc32f5e0cdb5
Jamie Cox <me@jcox.tech>
2019-03-18 12:47:59 +0000

    Add script for running tests against GKE

---
+++ run_gcp.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+if [ -z "$ENV" ]; then
+	ENV=ci
+fi
+
+GCP_PROJECT=census-rm-$ENV
+GCP_REGION=europe-west2
+
+if [ -z "$IMAGE" ]; then
+    IMAGE=eu.gcr.io/$GCP_PROJECT/rm/census-rm-acceptance-tests:latest
+fi
+
+if [ "$PUSH" = "true" ]; then
+    echo "Building and pushing docker image [$IMAGE]..."
+    docker build -t $IMAGE .
+    docker push $IMAGE
+fi
+
+echo "Using Acceptance Tests image [$IMAGE]"
+
+KUBERNETES_NAMESPACE=response-management-$ENV
+
+gcloud config set project $GCP_PROJECT
+gcloud container clusters get-credentials rm-k8s-cluster \
+    --region $GCP_REGION \
+    --project $GCP_PROJECT
+
+echo "Running RM Acceptance Tests in GKE [$GCP_PROJECT/rm-k8s-cluster/$KUBERNETES_NAMESPACE]..."
+
+kubectl run acceptance-tests -it --command --rm --quiet --generator=run-pod/v1 \
+    --image=$IMAGE --restart=Never \
+    $(while read env; do echo --env=${env}; done < kubernetes.env) \
+    --env=SFTP_USERNAME=$(kubectl get secret sftp-credentials -o=jsonpath="{.data.username}" --namespace=${KUBERNETES_NAMESPACE} | base64 --decode) \
+    --env=SFTP_PASSWORD=$(kubectl get secret sftp-credentials -o=jsonpath="{.data.password}" --namespace=${KUBERNETES_NAMESPACE} | base64 --decode) \
+    --env=REDIS_SERVICE_HOST=$(kubectl get configmap redis-config -o=jsonpath="{.data.redis-host}" --namespace=${KUBERNETES_NAMESPACE}) \
+    --env=REDIS_SERVICE_PORT=$(kubectl get configmap redis-config -o=jsonpath="{.data.redis-port}" --namespace=${KUBERNETES_NAMESPACE}) \
+    --namespace=$KUBERNETES_NAMESPACE \
+    -- /bin/bash -c "sleep 2; behave acceptance_tests/features"
