6487e8ddff6a940390b453aacb592efbf8917ccc
LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
2019-04-30 09:30:38 +0100

    Rmc 111 manifest file (#44)
    * wip
    
    * have 'expected' failing test
    
    * need to alter tests to look for correct filename, csv only for print file checks
    
    * fixed dir
    
    * tests successfully check manifest files, needs refactoring/clean up
    
    * refactored a little, pre merge master in
    
    * removed commented out code
    
    * Update acceptance_tests/features/steps/print_file_steps.py
    
    Co-Authored-By: LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
    
    * Update acceptance_tests/utilities/sftp_utility.py
    
    Co-Authored-By: LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
    
    * Update acceptance_tests/utilities/sftp_utility.py
    
    Co-Authored-By: LukeLoze1 <44841627+LukeLoze1@users.noreply.github.com>
    
    * fixed PR issues

--- acceptance_tests/features/steps/print_file_steps.py
+++ acceptance_tests/features/steps/print_file_steps.py
@@ -1,19 +1,30 @@
+import hashlib
+import json
 import logging
+from unittest import TestCase
 
 from behave import then
 from retrying import retry
 from structlog import wrap_logger
-
 from acceptance_tests.utilities.print_file_helper import create_expected_csv_lines
 from acceptance_tests.utilities.sftp_utility import SftpUtility
+from config import Config
 
 logger = wrap_logger(logging.getLogger(__name__))
+
+tc = TestCase('__init__')
 
 
 @then('correctly formatted print files are created')
 def check_correct_files_on_sftp_server(context):
     expected_csv_lines = create_expected_csv_lines(context)
     _check_notification_files_have_all_the_expected_data(context, expected_csv_lines)
+
+
+@then("there is a correct manifest file for each csv file written")
+def check_manifest_files(context):
+    logger.debug("checking manifest files exist for csv files")
+    _check_manifest_files_created(context)
 
 
 def _check_notification_files_have_all_the_expected_data(context, expected_csv_lines):
@@ -25,7 +36,8 @@ def _check_notification_files_have_all_the_expected_data(context, expected_csv_l
 def _validate_print_file_content(sftp_utility, start_of_test, expected_csv_lines):
     logger.debug('Checking for files on SFTP server')
 
-    files = sftp_utility.get_all_print_files_after_time(start_of_test)
+    files = sftp_utility.get_all_files_after_time(start_of_test, ".csv")
+
     actual_content_list = sftp_utility.get_files_content_as_list(files)
 
     if set(actual_content_list) != set(expected_csv_lines):
@@ -35,3 +47,62 @@ def _validate_print_file_content(sftp_utility, start_of_test, expected_csv_lines
         raise FileNotFoundError
 
     return True
+
+
+def _check_manifest_files_created(context):
+    with SftpUtility() as sftp_utility:
+        files = sftp_utility.get_all_files_after_time(context.test_start_local_datetime)
+
+        for _file in files:
+            if _file.filename.endswith(".csv"):
+                csv_file = _file
+                manifest_file = _get_matching_manifest_file(csv_file.filename, files)
+
+                if manifest_file is None:
+                    assert False, f'Failed to find manifest file for {csv_file.filename}'
+
+                actual_manifest = _get_actual_manifest(sftp_utility, manifest_file)
+                creation_datetime = actual_manifest['manifestCreated']
+                expected_manifest = _create_expected_manifest(sftp_utility, csv_file, creation_datetime)
+                tc.assertDictEqual(actual_manifest, expected_manifest)
+
+
+def _get_actual_manifest(sftp_utility, manifest_file):
+    actual_manifest_json = sftp_utility.get_file_contents_as_string(f'{Config.SFTP_DIR}/{manifest_file.filename}')
+    return json.loads(actual_manifest_json)
+
+
+def _get_matching_manifest_file(filename, files):
+    manifest_filename = filename.replace(".csv", ".manifest")
+
+    for _file in files:
+        if _file.filename == manifest_filename:
+            return _file
+
+    return None
+
+
+def _create_expected_manifest(sftp_utility, csv_file, created_datetime):
+    actual_file_contents = sftp_utility.get_file_contents_as_string(f'{Config.SFTP_DIR}/{csv_file.filename}')
+
+    md5_hash = hashlib.md5(actual_file_contents.encode('utf-8')).hexdigest()
+    expected_size = sftp_utility.get_file_size(f'{Config.SFTP_DIR}/{csv_file.filename}')
+
+    _file = dict(
+        sizeBytes=expected_size,
+        md5sum=md5_hash,
+        relativePath='.\\',
+        name=csv_file.filename
+    )
+
+    manifest = dict(
+        schemaVersion=1,
+        files=[_file],
+        sourceName="ONS_RM",
+        manifestCreated=created_datetime,
+        description="Initial contact letter households - England",
+        dataset="PPD1.1",
+        version=1
+    )
+
+    return manifest
