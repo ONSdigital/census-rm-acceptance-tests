552b0b041fab33e466c8dea2599f7b3026f387f4
Richard Weeks <richard.weeks@qa.com>
2019-05-13 08:56:57 +0100

    Add different treatment codes.

--- acceptance_tests/features/steps/action_steps.py
+++ acceptance_tests/features/steps/action_steps.py
@@ -13,6 +13,27 @@ def setup_action_rule(context, action_type, action_rule_delay):
     action_plan_url = action_plan_response_body['_links']['self']['href']
 
     trigger_date_time = (datetime.utcnow() + timedelta(seconds=int(action_rule_delay))).isoformat() + 'Z'
-    classifiers = {'treatmentCode': ['HH_LF3R2E', 'HH_LF3R3AE', 'HH_LF3R3BE', 'HH_LFNR1E', 'HH_LF2R3BE']}
+
+    if action_type == 'ICL1E':
+        classifiers = _get_england_icl_treatment_codes()
+
+    if action_type == 'ICL2E':
+        classifiers = _get_wales_icl_treatment_codes()
+
+    if action_type == 'ICHHQW':
+        classifiers = _get_wales_questionnaire_treatment_codes()
 
     create_action_rule(str(uuid.uuid4()), trigger_date_time, classifiers, action_plan_url, action_type)
+
+
+def _get_england_icl_treatment_codes():
+    return {'treatmentCode': ['HH_LF3R2E', 'HH_LF3R3AE', 'HH_LF3R3BE', 'HH_LFNR1E', 'HH_LF2R3BE']}
+
+
+def _get_wales_icl_treatment_codes():
+    return {'treatmentCode': ['HH_LFNR1W', 'HH_LFNR2W', 'HH_LFNR3AW', 'HH_LF2R1W', 'HH_LF2R2W', 'HH_LF2R3AW',
+                              'HH_LF2R3BW', 'HH_LF3R1W', 'HH_LF3R2W', 'HH_LF3R3AW', 'HH_LF3R3BW']}
+
+
+def _get_wales_questionnaire_treatment_codes():
+    return {'treatmentCode': ['HH_QF2R1E', 'HH_QF2R2E', 'HH_QF2R3AE', 'HH_QF3R1E', 'HH_QF3R2E', 'HH_QF3R3AE']}
