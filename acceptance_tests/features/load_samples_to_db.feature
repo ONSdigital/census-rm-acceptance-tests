3ca513a245a2565b169674d74317e868723e8b00
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-05-14 13:31:27 +0100

    Spike changes

--- acceptance_tests/features/load_samples_to_db.feature
+++ acceptance_tests/features/load_samples_to_db.feature
@@ -2,5 +2,5 @@ Feature: Load Samples from file to database
 
   Scenario: Successful sample file upload
     Given sample file "sample_input_census_spec.csv" is loaded
-    Then the new cases are emitted to Respondent Home
-    And the QID UAC pairs are emitted to Respondent Home
+#    Then the new cases are emitted to Respondent Home
+#    And the QID UAC pairs are emitted to Respondent Home
--- config.py
+++ config.py
@@ -40,7 +40,7 @@ class Config:
     RABBITMQ_PORT = os.getenv('RABBITMQ_SERVICE_PORT', '6672')
     RABBITMQ_VHOST = os.getenv('RABBITMQ_VHOST', '/')
     RABBITMQ_QUEUE = os.getenv('RABBITMQ_QUEUE', 'exampleInboundQueue')
-    RABBITMQ_RH_OUTBOUND_QUEUE = os.getenv('RABBITMQ_RH_OUTBOUND_QUEUE', 'myfanout.rhqueue')
+    RABBITMQ_RH_OUTBOUND_QUEUE = os.getenv('RABBITMQ_RH_OUTBOUND_QUEUE', 'mytopic.rhqueue.case')
     RABBITMQ_EXCHANGE = os.getenv('RABBITMQ_EXCHANGE', '')
     RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'guest')
     RABBITMQ_PASSWORD = os.getenv('RABBITMQ_PASSWORD', 'guest')
