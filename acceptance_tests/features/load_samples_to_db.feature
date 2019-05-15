1c9d6a4b3acc71aa7086c05b117eacb84879afa4
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2019-05-15 10:17:35 +0100

    Add test: check 2 uac updated messages emitted to RH for Wales questionnaires (#49)

--- acceptance_tests/features/load_samples_to_db.feature
+++ acceptance_tests/features/load_samples_to_db.feature
@@ -1,6 +1,11 @@
 Feature: Load Samples from file to database
 
   Scenario: Successful sample file upload
-    Given sample file "sample_input_census_spec.csv" is loaded
+    Given sample file "sample_input_england_census_spec.csv" is loaded
     Then the new cases are emitted to Respondent Home
     And the QID UAC pairs are emitted to Respondent Home
+
+  Scenario: Successful sample file upload Welsh questionnaire
+    Given sample file "sample_input_census_spec_wales_questionnaire.csv" is loaded
+    Then the new cases are emitted to Respondent Home
+    And two Wales QID UAC pairs are emitted to Respondent Home
--- acceptance_tests/utilities/print_file_helper.py
+++ acceptance_tests/utilities/print_file_helper.py
@@ -27,9 +27,59 @@ def create_expected_csv_lines(context, prefix):
     ]
 
 
+def create_expected_wales_csv_lines(context, prefix):
+    actual_data = defaultdict(dict)
+
+    for message in context.messages_received:
+        if message['event']['type'] == 'UAC_UPDATED':
+            if message['payload']['uac']['questionnaireId'][:2] == '02':
+                actual_data[message['payload']['uac']['caseId']]['uac'] = message['payload']['uac']['uac']
+                actual_data[message['payload']['uac']['caseId']]['qid'] = message['payload']['uac']['questionnaireId']
+            elif message['payload']['uac']['questionnaireId'][:2] == '03':
+                actual_data[message['payload']['uac']['caseId']]['uac_wales'] = message['payload']['uac']['uac']
+                actual_data[message['payload']['uac']['caseId']]['qid_wales'] \
+                    = message['payload']['uac']['questionnaireId']
+            else:
+                assert False, "Unexpected questionnaire type"
+        elif message['event']['type'] == 'CASE_CREATED':
+            actual_data[message['payload']['collectionCase']['id']]['case_ref'] = message['payload']['collectionCase'][
+                'caseRef']
+            actual_data[message['payload']['collectionCase']['id']]['address_line_1'] = \
+                message['payload']['collectionCase']['address']['addressLine1']
+            actual_data[message['payload']['collectionCase']['id']]['address_line_2'] = \
+                message['payload']['collectionCase']['address']['addressLine2']
+            actual_data[message['payload']['collectionCase']['id']]['address_line_3'] = \
+                message['payload']['collectionCase']['address']['addressLine3']
+            actual_data[message['payload']['collectionCase']['id']]['town_name'] = \
+                message['payload']['collectionCase']['address']['townName']
+            actual_data[message['payload']['collectionCase']['id']]['postcode'] = \
+                message['payload']['collectionCase']['address']['postcode']
+
+    return [
+        _create_expected_wales_csv_line(case, prefix)
+        for case in actual_data.values()
+    ]
+
+
 def _create_expected_csv_line(case, prefix):
     return (
         f'{case["uac"]}|'
+        f'{case["case_ref"]}|'
+        f'{case["address_line_1"]}|'
+        f'{case["address_line_2"]}|'
+        f'{case["address_line_3"]}|'
+        f'{case["town_name"]}|'
+        f'{case["postcode"]}|'
+        f'{prefix}'
+    )
+
+
+def _create_expected_wales_csv_line(case, prefix):
+    return (
+        f'{case["uac"]}|'
+        f'{case["qid"]}|'
+        f'{case["uac_wales"]}|'
+        f'{case["qid_wales"]}|'
         f'{case["case_ref"]}|'
         f'{case["address_line_1"]}|'
         f'{case["address_line_2"]}|'
