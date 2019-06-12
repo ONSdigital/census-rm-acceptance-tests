43991843e460de5230b1f66c27420c79fe528876
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-06-12 10:41:46 +0100

    removed docs from utility code

--- acceptance_tests/utilities/rabbit_helper.py
+++ acceptance_tests/utilities/rabbit_helper.py
@@ -46,19 +46,9 @@ def store_all_msgs_in_context(ch, method, _properties, body, context, expected_m
 
 
 def add_test_queue(rabbitmq_amqp, binding_key, exchange_name, queue_name, exchange_type='topic'):
-    """
-    Initialise connection to rabbitmq
-
-    :param rabbitmq_amqp: The amqp (url) of the rabbitmq connection
-    :param exchange_name: The rabbitmq exchange to publish to, (e.g.: "case-outbound-exchange")
-    :param queue_name: The rabbitmq queue that subscribes to the exchange, (e.g.: "Case.Responses")
-    :param binding_key: The binding key to associate the exchange and queue (e.g.: "Case.Responses.binding")
-    :param queue_args: Arguments passed to the rabbitmq queue declaration
-    """
-    logger.debug('Connecting to rabbitmq', url=rabbitmq_amqp)
     rabbitmq_connection = pika.BlockingConnection(pika.URLParameters(rabbitmq_amqp))
     channel = rabbitmq_connection.channel()
     channel.exchange_declare(exchange=exchange_name, exchange_type=exchange_type, durable=True)
     channel.queue_declare(queue=queue_name, durable=True)
     channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=binding_key)
-    logger.info('Successfully initialised rabbitmq', exchange=exchange_name, binding=binding_key)
+    logger.info('Successfully add test queue to rabbitmq', exchange=exchange_name, binding=binding_key)
