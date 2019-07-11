2323105f0c22fd3d7031780ed0bc7494d57fbe11
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2019-07-11 13:58:21 +0100

    decrypting sftp files (#81)
    * Decrypting SFTP files
    
    * Added mappings for different sftp directories

--- acceptance_tests/utilities/sftp_utility.py
+++ acceptance_tests/utilities/sftp_utility.py
@@ -2,7 +2,9 @@ import base64
 from datetime import datetime
 
 import paramiko
+import pgpy
 
+from acceptance_tests.utilities.mappings import PACK_CODE_TO_SFTP_DIRECTORY
 from config import Config
 
 
@@ -33,7 +35,7 @@ class SftpUtility:
         self.ssh_client.close()
 
     def get_all_files_after_time(self, period_start_time, prefix, suffix=""):
-        files = self._sftp_client.listdir_attr(Config.SFTP_DIR)
+        files = self._sftp_client.listdir_attr(PACK_CODE_TO_SFTP_DIRECTORY[prefix])
         period = period_start_time.strftime('%Y-%m-%d')
 
         return [
@@ -43,11 +45,11 @@ class SftpUtility:
                 and period_start_time <= datetime.fromtimestamp(_file.st_mtime)
             ]
 
-    def get_files_content_as_list(self, files):
+    def get_files_content_as_list(self, files, prefix):
         actual_content = []
 
         for _file in files:
-            file_path = f'{Config.SFTP_DIR}/{_file.filename}'
+            file_path = f'{PACK_CODE_TO_SFTP_DIRECTORY[prefix]}/{_file.filename}'
             content_list = self._get_file_lines_as_list(file_path)
             actual_content.extend(content_list)
 
@@ -56,7 +58,8 @@ class SftpUtility:
     def _get_file_lines_as_list(self, file_path):
         with self._sftp_client.open(file_path) as sftp_file:
             content = sftp_file.read().decode('utf-8')
-            return content.rstrip().split('\n')
+            decrypted_content = self.decrypt_message(content)
+            return decrypted_content.rstrip().split('\n')
 
     def get_file_contents_as_string(self, file_path):
         with self._sftp_client.open(file_path) as sftp_file:
@@ -64,3 +67,11 @@ class SftpUtility:
 
     def get_file_size(self, file_path):
         return self._sftp_client.lstat(file_path).st_size
+
+    def decrypt_message(self, message):
+        our_key, _ = pgpy.PGPKey.from_file('our_dummy_private.asc')
+        with our_key.unlock('test'):
+            encrypted_text_message = pgpy.PGPMessage.from_blob(message)
+            message_text = our_key.decrypt(encrypted_text_message)
+
+            return message_text.message
