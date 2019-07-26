98535298822bbda0ff9750e227660a535f6b5679
liamtoozer <liam.toozer@gmail.com>
2019-07-26 20:22:53 +0100

    Use existing FWMT step

--- acceptance_tests/features/refusal.feature
+++ acceptance_tests/features/refusal.feature
@@ -1,9 +1,10 @@
 Feature: Handle refusal message
 
-  Scenario: Refusal message results case excluded from action plan
+  Scenario: Refusal message results in case excluded from action plan
     Given an action rule of type ICL1E is set 10 seconds in the future
-    When sample file "sample_for_refusals.csv" is loaded
-    Then messages are emitted to RH and Action Scheduler with [01] qids
-    And a refusal message for a created case is received
+    And sample file "sample_for_refusals.csv" is loaded
+    And messages are emitted to RH and Action Scheduler with [01] qids
+    When a refusal message for a created case is received
+    Then only unrefused cases appear in "P_IC_ICL1" print files
     And the case is marked as refused
-    And only unrefused cases appear in "P_IC_ICL1" print files
+    And an action instruction cancel message is emitted to FWMT
--- acceptance_tests/features/steps/refusal.py
+++ acceptance_tests/features/steps/refusal.py
@@ -1,18 +1,15 @@
-import functools
 import json
-import xml.etree.ElementTree as ET
 
 import requests
-from behave import then
+from behave import step
 
 from acceptance_tests.utilities.rabbit_context import RabbitContext
-from acceptance_tests.utilities.rabbit_helper import start_listening_to_rabbit_queue
 from config import Config
 
 caseapi_url = f'{Config.CASEAPI_SERVICE}/cases/'
 
 
-@then("a refusal message for a created case is received")
+@step("a refusal message for a created case is received")
 def create_refusal(context):
     context.refused_case_id = context.uac_created_events[0]['payload']['uac']['caseId']
     context.refused_questionnaire_id = context.uac_created_events[0]['payload']['uac']['questionnaireId']
@@ -24,7 +21,7 @@ def create_refusal(context):
                 "source": "CONTACT_CENTRE_API",
                 "channel": "CC",
                 "dateTime": "2019-07-07T22:37:11.988+0000",
-                "transactionId": "a6f9f129-f582-4011-8b48-d787c45cab26"
+                "transactionId": "d2541acb-230a-4ade-8123-eee2310c9143"
             },
             "payload": {
                 "refusal": {
@@ -55,13 +52,14 @@ def create_refusal(context):
         }
     )
 
-    with RabbitContext(queue_name=Config.RABBITMQ_INBOUND_REFUSAL_QUEUE) as rabbit:
+    with RabbitContext(exchange=Config.RABBITMQ_EVENT_EXCHANGE) as rabbit:
         rabbit.publish_message(
             message=message,
-            content_type='application/json')
+            content_type='application/json',
+            routing_key=Config.RABBITMQ_REFUSAL_ROUTING_KEY)
 
 
-@then("the case is marked as refused")
+@step("the case is marked as refused")
 def check_case_events(context):
     response = requests.get(f'{caseapi_url}{context.refused_case_id}', params={'caseEvents': True})
     response_json = response.json()
@@ -69,28 +67,3 @@ def check_case_events(context):
         if case_event['description'] == 'Refusal Received':
             return
     assert False
-
-
-@then("the case is excluded from FWMT action rule")
-def check_case_excluded_from_action_rule(context):
-    context.expected_sample_units = [
-        sample_unit
-        for sample_unit in context.sample_units.copy() if sample_unit['attributes']['TREATMENT_CODE'] == 'HH_QF2R1E'
-    ]
-
-    start_listening_to_rabbit_queue(Config.RABBITMQ_OUTBOUND_FIELD_QUEUE_TEST,
-                                    functools.partial(_callback, context=context))
-
-    assert not context.expected_sample_units, 'Some messages are missing'
-
-
-def _callback(ch, method, _properties, body, context):
-    root = ET.fromstring(body)
-
-    if not root[0].tag == 'actionRequest':
-        ch.basic_nack(delivery_tag=method.delivery_tag)
-        assert False, 'Unexpected message on Action.Field case queue'
-
-    for sample_unit in context.expected_sample_units:
-        if sample_unit['id'] == root.find('.//caseId').text:
-            assert False, f"Unexpected caseId {sample_unit['id']} found in Action.Field message"
