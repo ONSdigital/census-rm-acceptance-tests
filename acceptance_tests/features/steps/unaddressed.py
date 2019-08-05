c6c169ffc478dfad175471404c4165d8ebd802d4
AdeHarrison <adrian.harrison.uk@gmail.com>
2019-08-05 11:51:24 +0100

    Added questionnaire linked acceptance test

--- acceptance_tests/features/steps/unaddressed.py
+++ acceptance_tests/features/steps/unaddressed.py
@@ -23,11 +23,48 @@ def send_unaddressed_message(context, questionnaire_type):
             content_type='application/json')
 
 
+@then("an UACUpdated message of questionnaire type {questionnaire_type} linking a questionnaire to a case is sent")
+def check_linked_message_is_received(context, questionnaire_type):
+    context.linked_case = context.case_created_events[1]['payload']['collectionCase']
+    context.linked_uac = context.uac_created_events[0]['payload']['uac']
+    context.expected_questionnaire_type = questionnaire_type
+
+    questionnaire_linked_message = {
+        'event': {
+            'type': 'RESPONSE_RECEIVED',
+            'source': 'FIELDWORK_GATEWAY',
+            'channel': 'FIELD',
+            "dateTime": "2011-08-12T20:17:46.384Z",
+            "transactionId": "c45de4dc-3c3b-11e9-b210-d663bd873d93"
+        },
+        'payload': {
+            'uac': {
+                "caseId": context.linked_case['id'],
+                'questionnaireId': context.linked_uac['questionnaireId']
+            }
+        }
+    }
+
+    with RabbitContext(queue_name=Config.RABBITMQ_QUESTIONNAIRE_LINKED_QUEUE) as rabbit:
+        rabbit.publish_message(
+            message=json.dumps(questionnaire_linked_message),
+            content_type='application/json')
+
+
 @then("a UACUpdated message not linked to a case is emitted to RH and Action Scheduler")
 def check_uac_message_is_received(context):
     context.expected_message_received = False
     start_listening_to_rabbit_queue(Config.RABBITMQ_RH_OUTBOUND_UAC_QUEUE_TEST,
                                     functools.partial(_uac_callback, context=context))
+
+    assert context.expected_message_received
+
+
+@then("a UACUpdated message linking a Questionnaire to a case is emitted to RH")
+def check_questionnaire_linked_message_is_received(context):
+    context.expected_message_received = False
+    start_listening_to_rabbit_queue(Config.RABBITMQ_RH_OUTBOUND_UAC_QUEUE_TEST,
+                                    functools.partial(_questionnaire_linked_callback, context=context))
 
     assert context.expected_message_received
 
@@ -42,6 +79,20 @@ def _uac_callback(ch, method, _properties, body, context):
     tc.assertEqual(64, len(parsed_body['payload']['uac']['uacHash']))
     tc.assertEqual(context.expected_questionnaire_type, parsed_body['payload']['uac']['questionnaireId'][:2])
     tc.assertIsNone(parsed_body['payload']['uac']['caseId'])
+    context.expected_message_received = True
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    ch.stop_consuming()
+
+
+def _questionnaire_linked_callback(ch, method, _properties, body, context):
+    parsed_body = json.loads(body)
+
+    if not parsed_body['event']['type'] == 'UAC_UPDATED':
+        ch.basic_nack(delivery_tag=method.delivery_tag)
+        return
+
+    tc.assertEqual(context.expected_questionnaire_type, parsed_body['payload']['uac']['questionnaireId'][:2])
+    tc.assertEqual(context.linked_case['id'], parsed_body['payload']['uac']['caseId'])
     context.expected_message_received = True
     ch.basic_ack(delivery_tag=method.delivery_tag)
     ch.stop_consuming()
--- acceptance_tests/features/unaddressed.feature
+++ acceptance_tests/features/unaddressed.feature
@@ -1,7 +1,14 @@
 Feature: Generating UAC/QID pairs for unaddressed letters & questionnaires
+
   Scenario: UAC/QID pair generated for unaddressed
     When an unaddressed message of questionnaire type 01 is sent
     Then a UACUpdated message not linked to a case is emitted to RH and Action Scheduler
+
+  Scenario: Questionnaire linked to unaddressed
+    Given sample file "sample_for_questionnaire_linked.csv" is loaded
+    And messages are emitted to RH and Action Scheduler with [01] qids
+    Then an UACUpdated message of questionnaire type 01 linking a questionnaire to a case is sent
+    And a UACUpdated message linking a Questionnaire to a case is emitted to RH
 
   @local-docker
   Scenario: Correct print files generated
