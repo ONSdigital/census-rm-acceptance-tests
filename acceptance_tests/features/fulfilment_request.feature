13362a6274c43e4111b9575e1e5fbe7a3dbd56d4
Hugh Brace <hugh.ons@gmail.com>
2019-08-14 11:24:51 +0100

    Adding acceptance test for GOV notify fulfilment request

--- acceptance_tests/features/fulfilment_request.feature
+++ acceptance_tests/features/fulfilment_request.feature
@@ -5,3 +5,11 @@ Feature: Handle fulfilment request events
     And messages are emitted to RH and Action Scheduler with [01] qids
     When a fulfilment request message for a created case is sent
     Then a fulfilment request event is logged
+
+
+  Scenario: Log event when a fulfilment request event is received
+    Given sample file "sample_input_england_census_spec.csv" is loaded
+    And messages are emitted to RH and Action Scheduler with [01] qids
+    When a UAC fulfilment request message for a created case is sent
+    Then a fulfilment request event is logged
+    And notify api was called
--- acceptance_tests/features/steps/fulfilment_request.py
+++ acceptance_tests/features/steps/fulfilment_request.py
@@ -8,10 +8,11 @@ from acceptance_tests.utilities.rabbit_context import RabbitContext
 from config import Config
 
 caseapi_url = f'{Config.CASEAPI_SERVICE}/cases/'
+notify_stub_url = f'{Config.NOTIFY_STUB_SERVICE}'
 
 
 @step("a fulfilment request message for a created case is sent")
-def create_refusal(context):
+def create_fulfilment_message(context):
     context.fulfilment_requested_case_id = context.uac_created_events[0]['payload']['uac']['caseId']
 
     message = json.dumps(
@@ -44,6 +45,39 @@ def create_refusal(context):
             routing_key=Config.RABBITMQ_FULFILMENT_REQUESTED_ROUTING_KEY)
 
 
+@step("a UAC fulfilment request message for a created case is sent")
+def create_uac_fulfilment_message(context):
+    requests.get(f'{notify_stub_url}/reset')
+    context.fulfilment_requested_case_id = context.uac_created_events[0]['payload']['uac']['caseId']
+
+    message = json.dumps(
+        {
+            "event": {
+                "type": "FULFILMENT_REQUESTED",
+                "source": "CONTACT_CENTRE_API",
+                "channel": "CC",
+                "dateTime": "2019-07-07T22:37:11.988+0000",
+                "transactionId": "d2541acb-230a-4ade-8123-eee2310c9143"
+            },
+            "payload": {
+                "fulfilmentRequest": {
+                    "fulfilmentCode": "UACHHT1",
+                    "caseId": context.fulfilment_requested_case_id,
+                    "contact": {
+                        "telNo": "01234567"
+                    }
+                }
+            }
+        }
+    )
+
+    with RabbitContext(exchange=Config.RABBITMQ_EVENT_EXCHANGE) as rabbit:
+        rabbit.publish_message(
+            message=message,
+            content_type='application/json',
+            routing_key=Config.RABBITMQ_FULFILMENT_REQUESTED_ROUTING_KEY)
+
+
 @step("a fulfilment request event is logged")
 def check_case_events(context):
     time.sleep(2)  # Give case processor a chance to process the fulfilment request event
@@ -53,3 +87,12 @@ def check_case_events(context):
         if case_event['description'] == 'Fulfilment Request Received':
             return
     assert False
+
+
+@step("notify api was called")
+def check_notify_api_call(context):
+    response = requests.get(f'{notify_stub_url}/log')
+    assert response.status_code == 200, "Unexpected status code"
+    response_json = response.json()
+    assert len(response_json) == 1, "Incorrect number of responses"
+    assert response_json[0]["phone_number"] == '01234567', "Invalid expected phone number"
