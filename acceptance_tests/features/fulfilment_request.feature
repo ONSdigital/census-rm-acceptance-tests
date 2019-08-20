d791d996088b2aa21f0097cb36418e724e86a5e5
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-08-20 11:05:18 +0100

    wip

--- acceptance_tests/features/fulfilment_request.feature
+++ acceptance_tests/features/fulfilment_request.feature
@@ -3,16 +3,25 @@ Feature: Handle fulfilment request events
   Scenario: Log event when a fulfilment request event is received
     Given sample file "sample_input_england_census_spec.csv" is loaded
     And messages are emitted to RH and Action Scheduler with [01] questionnaire types
-    When a UAC fulfilment request message for a created case is sent
+    When a UAC fulfilment request "UACHHT1" message for a created case is sent
     Then a fulfilment request event is logged
 
 
+#    Does this one differ from above apart from calling the notify api?
   Scenario: UAC fulfilment request event logged and sent to notify service
     Given sample file "sample_input_england_census_spec.csv" is loaded
     And messages are emitted to RH and Action Scheduler with [01] questionnaire types
-    When a UAC fulfilment request message for a created case is sent
+    When a UAC fulfilment request "UACHHT1" message for a created case is sent
     Then a fulfilment request event is logged
     And notify api was called
+
+  Scenario: Individual Response Fulfilment is received Log event without contact details, save new case, emit new case
+    Given sample file "sample_input_england_census_spec.csv" is loaded
+    And messages are emitted to RH and Action Scheduler with [01] questionnaire types
+    When a UAC fulfilment request "UACIT1" message for a created case is sent
+    Then a fulfilment request event is logged
+    And a new child case is emitted to RH and Action Scheduler
+#    notify called with correct template?
 
 
   Scenario Outline: Generate print file and log an event when an England fulfilment request event is received
--- acceptance_tests/features/steps/fulfilment_request.py
+++ acceptance_tests/features/steps/fulfilment_request.py
@@ -1,3 +1,4 @@
+import functools
 import json
 import time
 
@@ -5,8 +6,8 @@ import requests
 from behave import step
 
 from acceptance_tests.utilities.rabbit_context import RabbitContext
+from acceptance_tests.utilities.rabbit_helper import start_listening_to_rabbit_queue, store_all_msgs_in_context
 from config import Config
-
 
 notify_stub_url = f'{Config.NOTIFY_STUB_SERVICE}'
 get_cases_url = f'{Config.CASEAPI_SERVICE}/cases/'
@@ -51,8 +52,8 @@ def send_fulfilment_requested_event(context, pack_code):
             routing_key=Config.RABBITMQ_FULFILMENT_REQUESTED_ROUTING_KEY)
 
 
-@step("a UAC fulfilment request message for a created case is sent")
-def create_uac_fulfilment_message(context):
+@when('a UAC fulfilment request "{fulfilment_code}" message for a created case is sent')
+def create_uac_fulfilment_message(context, fulfilment_code):
     requests.get(f'{notify_stub_url}/reset')
     context.fulfilment_requested_case_id = context.uac_created_events[0]['payload']['uac']['caseId']
 
@@ -67,10 +68,11 @@ def create_uac_fulfilment_message(context):
             },
             "payload": {
                 "fulfilmentRequest": {
-                    "fulfilmentCode": "UACHHT1",
+                    "fulfilmentCode": fulfilment_code,
                     "caseId": context.fulfilment_requested_case_id,
                     "contact": {
-                        "telNo": "01234567"
+                        "telNo": "01234567",
+                        "address": "abc road"
                     }
                 }
             }
@@ -111,3 +113,23 @@ def check_case_events(context):
     assert 200 <= response.status_code <= 299, 'Get cases API call failed'
     cases = response.json()
     assert any(case_event['description'] == 'Fulfilment Request Received' for case_event in cases['caseEvents'])
+
+
+@step("a new child case is emitted to RH and Action Scheduler")
+def child_case_is_emitted(context):
+    context.messages_received = []
+
+    start_listening_to_rabbit_queue(Config.RABBITMQ_RH_OUTBOUND_CASE_QUEUE_TEST,
+                                    functools.partial(store_all_msgs_in_context, context=context,
+                                                      expected_msg_count=1,
+                                                      type_filter='CASE_CREATED'))
+    assert len(context.messages_received) == 1
+    child_case_arid = context.messages_received[0]['payload']['collectionCase']['address']['arid']
+    parent_case_arid = _get_parent_case_arid(context)
+
+    assert child_case_arid == parent_case_arid, "Parent and child Arids must match to link cases"
+
+
+def _get_parent_case_arid(context):
+    response = requests.get(f'{get_cases_url}{context.fulfilment_requested_case_id}')
+    return response.json()['arid']
