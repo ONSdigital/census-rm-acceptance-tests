d1f4b5fbe9c72ba33ef20198948ef726ee3a67d3
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-10-04 13:23:32 +0100

    new test for CCS with qid

--- acceptance_tests/features/steps/ccs_property_listed.py
+++ acceptance_tests/features/steps/ccs_property_listed.py
@@ -8,11 +8,63 @@ from behave import step, then
 from retrying import retry
 
 from acceptance_tests.utilities.rabbit_context import RabbitContext
-from acceptance_tests.utilities.rabbit_helper import start_listening_to_rabbit_queue
+from acceptance_tests.utilities.rabbit_helper import start_listening_to_rabbit_queue, store_all_msgs_in_context, \
+    check_no_msgs_sent_to_queue
 from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
 caseapi_url = f'{Config.CASEAPI_SERVICE}/cases/'
+
+
+@step("a CCS Property Listed event is sent with no qid")
+def send_ccs_property_listed_event_with_a_qid(context):
+    context.case_id = str(uuid.uuid4())
+
+    message = json.dumps({
+        "event": {
+            "type": "CCS_ADDRESS_LISTED",
+            "source": "FIELDWORK_GATEWAY",
+            "channel": "FIELD",
+            "dateTime": "2011-08-12T20:17:46.384Z",
+            "transactionId": "c45de4dc-3c3b-11e9-b210-d663bd873d93"
+        },
+        "payload": {
+            "CCSProperty": {
+                "collectionCase": {
+                    "id": context.case_id
+                },
+                "sampleUnit": {
+                    "addressType": "NR",
+                    "estabType": "Non-residential",
+                    "addressLevel": "U",
+                    "organisationName": "Testy McTest",
+                    "addressLine1": "123 Fake street",
+                    "addressLine2": "Upper upperingham",
+                    "addressLine3": "Newport",
+                    "townName": "upton",
+                    "postcode": "UP103UP",
+                    "latitude": "50.863849",
+                    "longitude": "-1.229710",
+                    "fieldcoordinatorId": "XXXXXXXXXX",
+                    "fieldofficerId": "XXXXXXXXXXXXX"
+                },
+                "refusal": {
+                    "type": "HARD_REFUSAL",
+                    "report": "respondent too busy",
+                    "agentId": "110001"
+                },
+                "invalidAddress": {
+                    "reason": "DEMOLISHED"
+                }
+            }
+        }
+    })
+
+    with RabbitContext(exchange=Config.RABBITMQ_EVENT_EXCHANGE) as rabbit:
+        rabbit.publish_message(
+            message=message,
+            content_type='application/json',
+            routing_key=Config.RABBITMQ_CCS_PROPERTY_LISTING_ROUTING_KEY)
 
 
 @step("a CCS Property Listed event is sent")
@@ -48,7 +100,7 @@ def send_ccs_property_listed_event(context):
                     "fieldofficerId": "XXXXXXXXXXXXX"
                 },
                 "uac": {
-                    "questionnaireId": "1110000009"
+                    "questionnaireId": context.expected_questionnaire_id
                 },
                 "refusal": {
                     "type": "HARD_REFUSAL",
@@ -107,3 +159,11 @@ def field_callback(ch, method, _properties, body, context):
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
     ch.stop_consuming()
+
+
+@then("no ActionInstruction is sent to FWMT")
+def check_no_msg_sent_fwmt(context):
+    check_no_msgs_sent_to_queue(Config.RABBITMQ_OUTBOUND_FIELD_QUEUE_TEST,
+                                functools.partial(
+                                    store_all_msgs_in_context, context=context,
+                                    expected_msg_count=0))
