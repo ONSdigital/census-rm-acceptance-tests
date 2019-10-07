e16fc7065751da5c38c859104c5774dabaaae70d
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2019-10-07 15:01:59 +0100

    deduplucate code to set up CCS listed event

--- acceptance_tests/features/ccs_property_listed.feature
+++ acceptance_tests/features/ccs_property_listed.feature
@@ -3,11 +3,11 @@ Feature: Handle CCS (Census Coverage Survey) Property Listed events
   Scenario: Log event when a CCS Property Listed event is received with no qid
     When a CCS Property Listed event is sent
     Then the CCS Property Listed case is created
-    Then the correct ActionInstruction is sent to FWMT
+    And the correct ActionInstruction is sent to FWMT
 
   Scenario: Log event when a CCS Property Listed event is received with a qid
     When an unaddressed message of questionnaire type 71 is sent
-    Then a UACUpdated message not linked to a case is emitted to RH and Action Scheduler
+    And a UACUpdated message not linked to a case is emitted to RH and Action Scheduler
     When a CCS Property Listed event is sent with a qid
-    Then the CCS Property Listed case is created
-    Then no ActionInstruction is sent to FWMT
+    And the CCS Property Listed case is created
+    And no ActionInstruction is sent to FWMT
--- acceptance_tests/features/steps/ccs_property_listed.py
+++ acceptance_tests/features/steps/ccs_property_listed.py
@@ -4,7 +4,7 @@ import uuid
 
 import xml.etree.ElementTree as ET
 import requests
-from behave import step, then
+from behave import step
 from retrying import retry
 
 from acceptance_tests.utilities.rabbit_context import RabbitContext
@@ -20,37 +20,7 @@ caseapi_url = f'{Config.CASEAPI_SERVICE}/cases/'
 def send_ccs_property_listed_event(context):
     context.case_id = str(uuid.uuid4())
 
-    message = json.dumps({
-        "event": {
-            "type": "CCS_ADDRESS_LISTED",
-            "source": "FIELDWORK_GATEWAY",
-            "channel": "FIELD",
-            "dateTime": "2011-08-12T20:17:46.384Z",
-            "transactionId": "c45de4dc-3c3b-11e9-b210-d663bd873d93"
-        },
-        "payload": {
-            "CCSProperty": {
-                "collectionCase": {
-                    "id": context.case_id
-                },
-                "sampleUnit": {
-                    "addressType": "HH",
-                    "estabType": "Household",
-                    "addressLevel": "U",
-                    "organisationName": "Testy McTest",
-                    "addressLine1": "123 Fake street",
-                    "addressLine2": "Upper upperingham",
-                    "addressLine3": "Newport",
-                    "townName": "upton",
-                    "postcode": "UP103UP",
-                    "latitude": "50.863849",
-                    "longitude": "-1.229710",
-                    "fieldcoordinatorId": "XXXXXXXXXX",
-                    "fieldofficerId": "XXXXXXXXXXXXX"
-                }
-            }
-        }
-    })
+    message = _get_ccs_property_listed_event(context.case_id)
 
     with RabbitContext(exchange=Config.RABBITMQ_EVENT_EXCHANGE) as rabbit:
         rabbit.publish_message(
@@ -62,8 +32,17 @@ def send_ccs_property_listed_event(context):
 @step("a CCS Property Listed event is sent with a qid")
 def send_ccs_property_listed_event_with_qid(context):
     context.case_id = str(uuid.uuid4())
+    message = _get_ccs_property_listed_event(context.case_id, context.expected_questionnaire_id)
 
-    message = json.dumps({
+    with RabbitContext(exchange=Config.RABBITMQ_EVENT_EXCHANGE) as rabbit:
+        rabbit.publish_message(
+            message=message,
+            content_type='application/json',
+            routing_key=Config.RABBITMQ_CCS_PROPERTY_LISTING_ROUTING_KEY)
+
+
+def _get_ccs_property_listed_event(case_id, questionnaire_id=None):
+    message = {
         "event": {
             "type": "CCS_ADDRESS_LISTED",
             "source": "FIELDWORK_GATEWAY",
@@ -74,7 +53,7 @@ def send_ccs_property_listed_event_with_qid(context):
         "payload": {
             "CCSProperty": {
                 "collectionCase": {
-                    "id": context.case_id
+                    "id": case_id
                 },
                 "sampleUnit": {
                     "addressType": "HH",
@@ -91,18 +70,16 @@ def send_ccs_property_listed_event_with_qid(context):
                     "fieldcoordinatorId": "XXXXXXXXXX",
                     "fieldofficerId": "XXXXXXXXXXXXX"
                 },
-                "uac": {
-                    "questionnaireId": context.expected_questionnaire_id
-                }
             }
         }
-    })
+    }
 
-    with RabbitContext(exchange=Config.RABBITMQ_EVENT_EXCHANGE) as rabbit:
-        rabbit.publish_message(
-            message=message,
-            content_type='application/json',
-            routing_key=Config.RABBITMQ_CCS_PROPERTY_LISTING_ROUTING_KEY)
+    if questionnaire_id:
+        message['payload']['CCSProperty']['uac'] = {
+            "questionnaireId": questionnaire_id
+        }
+
+    return json.dumps(message)
 
 
 @step("the CCS Property Listed case is created")
@@ -115,7 +92,7 @@ def check_case_created(context):
     assert context.ccs_case['caseType'] == 'HH'  # caseType is derived from addressType for CCS
 
 
-@then("the correct ActionInstruction is sent to FWMT")
+@step("the correct ActionInstruction is sent to FWMT")
 def check_correct_ccs_actioninstruction_sent_to_fwmt(context):
     context.messages_received = []
     start_listening_to_rabbit_queue(Config.RABBITMQ_OUTBOUND_FIELD_QUEUE_TEST,
@@ -145,7 +122,7 @@ def field_callback(ch, method, _properties, body, context):
     ch.stop_consuming()
 
 
-@then("no ActionInstruction is sent to FWMT")
+@step("no ActionInstruction is sent to FWMT")
 def check_no_msg_sent_fwmt(context):
     check_no_msgs_sent_to_queue(Config.RABBITMQ_OUTBOUND_FIELD_QUEUE_TEST,
                                 functools.partial(
