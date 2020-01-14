f6cef24b808672f2d340231fc8ac80b6ec08605b
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-01-14 09:04:53 +0000

    Remove unnecessary variable

--- acceptance_tests/features/steps/telephone_capture.py
+++ acceptance_tests/features/steps/telephone_capture.py
@@ -4,13 +4,11 @@ from behave import step
 from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
-case_api_url = f'{Config.CASEAPI_SERVICE}/cases/'
-
 
 @step("there is a request for telephone capture for an english respondent with case type HH")
 def request_telephone_capture_qid_uac(context):
     context.first_case = context.case_created_events[0]['payload']['collectionCase']
-    response = requests.get(f"{case_api_url}{context.first_case['id']}/qid")
+    response = requests.get(f"{Config.CASEAPI_SERVICE}/cases/{context.first_case['id']}/qid")
     test_helper.assertEqual(response.status_code, 200)
 
     context.telephone_capture_qid_uac = response.json()
