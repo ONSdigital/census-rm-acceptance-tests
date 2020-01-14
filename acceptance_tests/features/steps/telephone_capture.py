1c0aa0b1316258bd553ea75d7014b2ae9dc60f8b
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-01-14 09:20:57 +0000

    Improve feature wording and checking of UAC update event

--- acceptance_tests/features/steps/telephone_capture.py
+++ acceptance_tests/features/steps/telephone_capture.py
@@ -1,11 +1,14 @@
+import functools
+
 import requests
 from behave import step
 
+from acceptance_tests.utilities.rabbit_helper import start_listening_to_rabbit_queue, store_all_msgs_in_context
 from acceptance_tests.utilities.test_case_helper import test_helper
 from config import Config
 
 
-@step("there is a request for telephone capture for an english respondent with case type HH")
+@step('there is a request for telephone capture for an english respondent with case type HH')
 def request_telephone_capture_qid_uac(context):
     context.first_case = context.case_created_events[0]['payload']['collectionCase']
     response = requests.get(f"{Config.CASEAPI_SERVICE}/cases/{context.first_case['id']}/qid")
@@ -14,7 +17,25 @@ def request_telephone_capture_qid_uac(context):
     context.telephone_capture_qid_uac = response.json()
 
 
-@step("generate and return a UAC and the correct english HH QID type")
-def check_telephone_capture_uac_and_qid_type(context):
+@step('a UAC and QID with questionnaire type "{questionnaire_type}" type are generated and returned')
+def check_telephone_capture_uac_and_qid_type(context, questionnaire_type):
     test_helper.assertIsNotNone(context.telephone_capture_qid_uac.get('uac'))
-    test_helper.assertEqual(context.telephone_capture_qid_uac['questionnaireId'][:2], '01')
+    test_helper.assertEqual(context.telephone_capture_qid_uac['questionnaireId'][:2], questionnaire_type)
+
+
+@step('a UAC updated message linking the new UAC and QID to the requested case is emitted')
+def check_correct_uac_updated_message_is_emitted(context):
+    context.messages_received = []
+    start_listening_to_rabbit_queue(Config.RABBITMQ_RH_OUTBOUND_UAC_QUEUE_TEST,
+                                    functools.partial(store_all_msgs_in_context, context=context,
+                                                      expected_msg_count=1,
+                                                      type_filter='UAC_UPDATED'))
+
+    uac_updated_payload = context.messages_received[0]['payload']['uac']
+    test_helper.assertEqual(uac_updated_payload['caseId'], context.first_case['id'],
+                            'UAC updated event case ID does not match the case it was requested for')
+    test_helper.assertEqual(uac_updated_payload['uac'], context.telephone_capture_qid_uac['uac'],
+                            'UAC updated event UAC does not match what the API returned')
+    test_helper.assertEqual(uac_updated_payload['questionnaireId'],
+                            context.telephone_capture_qid_uac['questionnaireId'],
+                            'UAC updated event QID does not match what the API returned')
--- acceptance_tests/features/telephone_capture.feature
+++ acceptance_tests/features/telephone_capture.feature
@@ -6,5 +6,5 @@ Feature: Telephone capture
   Scenario: Generate correct QID type
     Given sample file "sample_1_english_unit.csv" is loaded successfully
     When there is a request for telephone capture for an english respondent with case type HH
-    Then generate and return a UAC and the correct english HH QID type
-    And a UAC updated message with "01" questionnaire type is emitted
+    Then a UAC and QID with questionnaire type "01" type are generated and returned
+    And a UAC updated message linking the new UAC and QID to the requested case is emitted
