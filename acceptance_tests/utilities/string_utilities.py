b4e75869220be68c5f78404acf85a97b2405b7e0
Adam <35268982+AdamHawtin@users.noreply.github.com>
2020-02-03 13:16:23 +0000

    RMC-221 CCS case look up by postcode (#168)

--- acceptance_tests/utilities/string_utilities.py
+++ acceptance_tests/utilities/string_utilities.py
@@ -1,7 +1,15 @@
 import string
-from random import choice, randint
+from random import choice, randint, random
 
 
-def create_random_string(min_len, max_len):
+def random_string_upper_or_digits(min_len, max_len):
     random_chars = string.ascii_uppercase + string.digits
     return "".join(choice(random_chars) for _ in range(randint(min_len, max_len)))
+
+
+def create_random_postcode():
+    return (f'{random_string_upper_or_digits(3, 4)}'
+            f'{" " if random() > 0.5 else ""}'
+            f'{choice(string.digits)}'
+            f'{choice(string.ascii_uppercase)}'
+            f'{choice(string.ascii_uppercase)}')
