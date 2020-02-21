2eeb3fec1b7d45917699c1558cacbe69129d37d7
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-02-21 15:05:20 +0000

    removed unused functions from receipt.py

--- acceptance_tests/features/steps/receipt.py
+++ acceptance_tests/features/steps/receipt.py
@@ -59,7 +59,7 @@ def uac_updated_msg_emitted(context):
 @step('an ActionCancelled Invalid Address event is sent to field work management with addressType "{address_type}"')
 def action_cancelled_event_sent_to_fwm(context, address_type):
     context.messages_received = []
-    start_listening_to_rabbit_queue(Config.RABBITMQ_OUTBOUND_FIELD_QUEUE_TEST, functools.partial(
+    start_listening_to_rabbit_queue(Config.RABBITMQ_OUTBOUND_FIELD_QUEUE, functools.partial(
         _field_work_receipt_callback, context=context))
 
     test_helper.assertEqual(context.fwmt_emitted_case_id, context.first_case["id"])
@@ -185,19 +185,6 @@ def _publish_offline_receipt(context, tx_id="3d14675d-a25d-4672-a0fe-b960586653e
     context.sent_to_gcp = True
 
 
-@when('we request Qid type "{}" case type "{}" country "{country_code}" receiving a "{expected_qid}"')
-def step_impl(context, qid_type, case_type, country_code, expected_qid):
-    if qid_type == 'Use Existing':
-        context.qid_to_receipt = context.uac_created_events[0]['payload']['uac']['questionnaireId']
-        return
-
-    if qid_type == 'Telephone':
-        request_individual_telephone_capture(context, case_type, country_code)
-        context.qid_to_receipt = context.telephone_capture_qid_uac
-        check_correct_uac_updated_message_is_emitted(context)
-        return
-
-
 @step('if required a new qid and case are created for "{case_type}" "{address_level}" "{qid_type}" "{country_code}"')
 def get_new_qid_and_case_as_required(context, case_type, address_level, qid_type, country_code):
     context.loaded_case = context.case_created_events[0]['payload']['collectionCase']
@@ -260,16 +247,6 @@ def check_receipt_to_field_msg(context, action_instruction_type, incremented):
     msg_to_field = context.messages_received[0]
     test_helper.assertEquals(msg_to_field['caseId'], context.receipting_case['id'])
     test_helper.assertEquals(msg_to_field['actionInstruction'], action_instruction_type)
-
-
-@step("a response received event is logged against the correct case")
-def check_response_received_logged_against_case(context):
-    actual_logged_events = get_logged_events_for_case_by_id(context.receipting_case['id'])
-
-    for case_event in actual_logged_events:
-        if case_event['eventType'] == 'RESPONSE_RECEIVED':
-            return
-    test_helper.fail('Did not find fulfilment request event')
 
 
 @step('the correct events are logged for "{loaded_case_events}" and "{individual_case_events}"')
