d54c270c8b5aba13f83c1c07fed2b6e5f3f88adf
Luke Loze <Luke.Loze@ext.ons.gov.uk>
2020-02-25 13:28:54 +0000

    passes flag to sample_loader to return sample_units, ups version of sample_loader called

--- acceptance_tests/features/steps/sample.py
+++ acceptance_tests/features/steps/sample.py
@@ -35,6 +35,7 @@ def load_sample_file_successfully_step(context, sample_file_name):
 def _load_sample(context, sample_file_name):
     sample_file_path = Path(__file__).parents[3].joinpath('resources', 'sample_files', sample_file_name)
     return load_sample_file(sample_file_path, context.collection_exercise_id, context.action_plan_id,
+                            store_loaded_sample_units=True,
                             host=Config.RABBITMQ_HOST, port=Config.RABBITMQ_PORT,
                             vhost=Config.RABBITMQ_VHOST, exchange=Config.RABBITMQ_EXCHANGE,
                             user=Config.RABBITMQ_USER, password=Config.RABBITMQ_PASSWORD,
