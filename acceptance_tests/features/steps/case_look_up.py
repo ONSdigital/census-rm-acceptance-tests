beda5483f15237524cb2ebc60e393a04a5b6518b
David Mort <davidmort100@gmail.com>
2020-03-09 14:05:10 +0000

    add tests for the re-instated caseType field in the case lookup api (#208)
    * add tests for the re-instated caseType field in the case lookup api
    
    * remove package_vulnerability step as its currently broke
    
    * remove package_vulnerability step from travis build as its currently broke

--- acceptance_tests/features/steps/case_look_up.py
+++ acceptance_tests/features/steps/case_look_up.py
@@ -104,6 +104,7 @@ def check_census_case_fields(context):
     test_helper.assertTrue(context.case_details['lad'])
     test_helper.assertTrue(context.case_details['id'])
     test_helper.assertTrue(context.case_details['addressType'])
+    test_helper.assertTrue(context.case_details['caseType'])
     test_helper.assertEqual(context.case_details['surveyType'], "CENSUS")
     test_helper.assertFalse(context.case_details['handDelivery'])
 
@@ -135,6 +136,7 @@ def check_ccs_case_fields(context):
     test_helper.assertFalse(context.ccs_case['lad'])
     test_helper.assertTrue(context.ccs_case['id'])
     test_helper.assertTrue(context.ccs_case['addressType'])
+    test_helper.assertTrue(context.ccs_case['caseType'])
     test_helper.assertFalse(context.ccs_case['handDelivery'])
 
 
