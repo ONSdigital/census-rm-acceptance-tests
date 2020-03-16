b4a72a3897c82c1c3a5ca022ba17e63504a5b0e4
Leo Howard <38526889+LeoKHoward@users.noreply.github.com>
2020-03-16 09:06:54 +0000

    Tranche ID changed (#212)

--- acceptance_tests/features/steps/case_look_up.py
+++ acceptance_tests/features/steps/case_look_up.py
@@ -49,7 +49,7 @@ def find_case_by_case_ref(context):
 def validate_ccs_qid_for_case_id(context):
     response_json = get_ccs_qid_for_case_id(context.ccs_case['id'])
     test_helper.assertEqual(response_json['questionnaireId'][0:3],
-                            '712', 'CCS QID has incorrect questionnaire type or tranche ID')
+                            '713', 'CCS QID has incorrect questionnaire type or tranche ID')
     test_helper.assertTrue(response_json['active'])
     test_helper.assertEqual(response_json['formType'], "H",
                             f'Expected FormType is "H" but got "{response_json["formType"]}"')
